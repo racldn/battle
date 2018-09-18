85534c3cec8e5d1ee17ca2388b36ebd7d8d8b8ef
Victor Jefferies <victorjefferies@MacBook-Pro-5.local>
2018-09-18 15:52:27 +0100

    testing forms

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -3,4 +3,17 @@ feature 'Testing infrastructure' do
     visit('/')
     expect(page).to have_content 'Testing infrastructure working!'
   end
+
+
+  screnario 'Testing player1 and player2 forms in capybara' do
+    visit('/battle')
+
+    fill_in 'player1',:with 'test'
+    click_on 'subp1'
+    expect(page).to have_content 'test'
+    fill_in 'player2', :with 'test2'
+    click_on 'subp2'
+    expect(page).to have_content 'test2'
+  end
 end
+
